f52a2865144ff186d390c62d02ad9f9cc7abcab5
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-11-22 22:20:18 -0200

    netflix implemented

---
+++ plugins/netflix/index.js
@@ -0,0 +1,22 @@
+var util = require("util");
+var exec = require("child_process").exec;
+
+var DEFAULT_BROWSER = "chromium-browser  --user-data-dir=/tmp/ --kiosk 'http://movies.netflix.com/WiSearch?raw_query=&v1=%s'";
+
+module.exports = function(params, fn) {
+
+	if(!params.length) {
+		throw new Error("Unknown or invalid browser params");
+	}
+
+	else if(typeof params === typeof "") {
+		params = [params];
+	}
+
+	var paramStr = params.join(" ");
+	var browserCommand = util.format(DEFAULT_BROWSER, encodeURI(paramStr));
+
+	exec(browserCommand, function(err, stdout, stderr) {
+		fn(err, stdout);
+	});
+}
